31cece5a4aee6a7e766a3f92f47d7dd04f3e7a04
HariMalode <hariomvm21@gmail.com>
2024-04-18 11:18:01 +0530

    auth and message controller added added

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,35 @@
+import Message from "../models/messageModel.js"
+import Conversation from "../models/conversationModel.js"
+
+
+export const sendMessage = async(req, res) => {
+   try {
+    const {message} = req.body
+    const {is:receiverId} = req.params
+    const senderId = req.user._id
+
+    let conversation = await Conversation.findOne({participants: {$all: [senderId, receiverId]}})
+
+    if(!conversation) {
+        conversation = await Conversation.create({participants: [senderId, receiverId]})
+    }
+
+    const newMessage = await Message.create({senderId, receiverId, message})
+
+    if(newMessage) {
+        conversation.messages.push(newMessage._id)
+    }
+
+    // await conversation.save()
+    // await newMessage.save()
+    
+    // this will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()])
+
+    res.status(201).json(newMessage)
+
+   } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message)
+        res.status(500).json({error: "Internal server error"})
+   }
+}
