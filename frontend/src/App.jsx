d91f3396d421f51a50cbf7c1073d260b18295faa
HariMalode <hariomvm21@gmail.com>
2024-07-07 21:10:39 +0530

    Frontend-UI Design Completed git add .

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
+import Home from './pages/home/Home.jsx'
+
+function App() {
+
+  return (
+    <>
+        <div className='p-4 h-screen flex items-center justify-center'>
+          {/* <SignUp/> */}
+          {/* <Login/> */}
+          <Home/>
+        </div>     
+    </>
+  )
+}
+
+export default App
