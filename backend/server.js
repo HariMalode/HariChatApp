21e0fcbda61508a22dcdc855a31e33b6a781800c
HariMalode <hariomvm21@gmail.com>
2024-07-10 17:01:05 +0530

    Frontend and Backend on single local Port 3k

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,13 @@ import userRoutes from './routes/userRoutes.js';
 import connectMongoDB from './db/connectMongoDB.js';
 import { app, server } from './socket/socket.js';
 
+import path from 'path';
+
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // parse incoming requests data as JSON from body
 app.use(cookieParser());
@@ -19,6 +23,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//must be at last middleware
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+});
 
 server.listen(PORT, () => {
   connectMongoDB();
